b692773ec41d69712c3805f87f2940bb458927ce
Jonmid <jonmid.mideros@gmail.com>
2018-10-31 15:42:38 -0500

    first commit

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ let futbol = [
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // ********************************************************************
 // ********************************************************************
 
